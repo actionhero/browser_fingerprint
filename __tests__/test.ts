fe21de5454c3cbd5af2b1a5841b56d05ad306fb1
Evan Tahler <evan@delicioushat.com>
2019-12-07 15:18:54 -0800

    typescript (#24)

---
+++ __tests__/test.ts
@@ -0,0 +1,186 @@
+import * as path from "path";
+import * as http from "http";
+import * as request from "request-promise-native";
+import { BrowserFingerprint } from "./../src/browserFingerprint";
+
+const port = 8081;
+const url = `http://localhost:${port}`;
+
+let fingerPrinter = new BrowserFingerprint();
+
+let server;
+let fingerprint;
+
+describe("browser fingerpint", () => {
+  beforeAll(() => {
+    server = http
+      .createServer((req, res) => {
+        const {
+          fingerprint,
+          elementHash,
+          headersHash
+        } = fingerPrinter.fingerprint(req);
+        headersHash["Content-Type"] = "text/plain";
+        res.writeHead(200, headersHash);
+        let resp = `Fingerprint: ${fingerprint} \r\n\r\n`;
+        for (const i in elementHash) {
+          resp += `Element ${i}: ${elementHash[i]}\r\n`;
+        }
+        res.end(resp);
+      })
+      .listen(port);
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("works with defaults", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    fingerprint = fingerprintCookie.split("=")[1];
+    expect(body).toContain(fingerprint);
+  });
+
+  it("will generate a new fingerprint for a new request", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const thisFingerprint = fingerprintCookie.split("=")[1];
+    expect(thisFingerprint).not.toBe(fingerprint);
+  });
+
+  it("can have a custom cookieKey", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const cookieKey = fingerprintCookie.split("=")[0];
+    expect(cookieKey).not.toBe("myCookie");
+  });
+
+  it("can have more entropy", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const thisFingerprint = fingerprintCookie.split("=")[1];
+    expect(thisFingerprint).not.toBe(fingerprint);
+  });
+
+  it("will return the same fingerprint if the cookie is already set via cookie, and not re-set the cookie", async () => {
+    const jar = request.jar();
+
+    const { body, headers } = await request.get(url, {
+      jar,
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const thisFingerprint = fingerprintCookie.split("=")[1];
+    expect(body).toContain(thisFingerprint);
+
+    const secondResponse = await request.get(url, {
+      jar,
+      resolveWithFullResponse: true
+    });
+
+    expect(secondResponse.headers["set-cookie"]).toBeUndefined();
+    expect(secondResponse.body).toContain(thisFingerprint);
+  });
+
+  it("will return the same fingerprint if the cookie is already set via header, and not re-set the cookie", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const thisFingerprint = fingerprintCookie.split("=")[1];
+    expect(body).toContain(thisFingerprint);
+
+    const secondResponse = await request.get(url, {
+      headers: {
+        __browser_fingerprint: thisFingerprint
+      },
+      resolveWithFullResponse: true
+    });
+
+    expect(secondResponse.headers["set-cookie"]).toBeUndefined();
+    expect(secondResponse.body).toContain(thisFingerprint);
+  });
+
+  it("will return the same fingerprint if the cookie is already set via x-header, and not re-set the cookie", async () => {
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const thisFingerprint = fingerprintCookie.split("=")[1];
+    expect(body).toContain(thisFingerprint);
+
+    const secondResponse = await request.get(url, {
+      headers: {
+        "x-__browser_fingerprint": thisFingerprint
+      },
+      resolveWithFullResponse: true
+    });
+
+    expect(secondResponse.headers["set-cookie"]).toBeUndefined();
+    expect(secondResponse.body).toContain(thisFingerprint);
+  });
+
+  it("can disable setting the cookie", async () => {
+    fingerPrinter = new BrowserFingerprint({ toSetCookie: false });
+
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    expect(headers["set-cookie"]).toBeUndefined();
+  });
+
+  it("works with directives without value", async () => {
+    const options = { settings: { httpOnly: null, secure: null } };
+    fingerPrinter = new BrowserFingerprint(options);
+
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const httpOnlyDirective = fingerprintCookie.split(";")[1];
+    const secureDirective = fingerprintCookie.split(";")[2];
+
+    expect(httpOnlyDirective).toBe("httpOnly");
+    expect(secureDirective).toBe("secure");
+  });
+
+  it("works with directives with and without value", async () => {
+    const options = {
+      settings: { expires: 3600000, httpOnly: null, path: "/", secure: null }
+    };
+    fingerPrinter = new BrowserFingerprint(options);
+
+    const { body, headers } = await request.get(url, {
+      resolveWithFullResponse: true
+    });
+
+    const fingerprintCookie = headers["set-cookie"][0];
+    const expiresDirective = fingerprintCookie.split(";")[1];
+    const httpOnlyDirective = fingerprintCookie.split(";")[2];
+    const pathDirective = fingerprintCookie.split(";")[3];
+    const secureDirective = fingerprintCookie.split(";")[4];
+
+    expect(expiresDirective).toContain("expires=");
+    expect(httpOnlyDirective).toBe("httpOnly");
+    expect(secureDirective).toBe("secure");
+    expect(pathDirective).toBe("path=/");
+  });
+});
---
+++ src/browserFingerprint.ts
@@ -0,0 +1,228 @@
+import { IncomingMessage } from "http";
+import * as crypto from "crypto";
+import * as os from "os";
+
+export class BrowserFingerprint {
+  options: {
+    cookieKey?: string;
+    toSetCookie?: boolean;
+    onlyStaticElements?: boolean;
+    settings?: { [key: string]: any };
+  };
+  savedHashedHostName: string;
+  savedHashedPid: number | string;
+
+  constructor(options = {}) {
+    this.options = options;
+    this.savedHashedHostName = null;
+    this.savedHashedPid = null;
+
+    this.hashedHostName();
+    this.hashedPid();
+  }
+
+  defaults() {
+    return {
+      cookieKey: "__browser_fingerprint",
+      toSetCookie: true,
+      onlyStaticElements: false
+    };
+  }
+
+  /**
+   * The goal is to come up with as many *potentially* unique traits for the connection and add them to the elementsHash.
+   * The collection of all the elementsHash keys will *hopefully* be unique enough for fingerprinting
+   * Then, we save this hash in a cookie for later retrieval
+   */
+  fingerprint(req: IncomingMessage) {
+    let fingerprint: string;
+    let key: string;
+    let value: string;
+    const cookies = this.parseCookies(req);
+
+    const defaults = this.defaults();
+    for (const i in defaults) {
+      if (this.options[i] == null) {
+        this.options[i] = defaults[i];
+      }
+    }
+
+    if (cookies[this.options.cookieKey] != null) {
+      fingerprint = cookies[this.options.cookieKey];
+      return {
+        fingerprint,
+        elementHash: { clientCookie: fingerprint },
+        headersHash: {}
+      };
+    }
+
+    if (req.headers[this.options.cookieKey] != null) {
+      fingerprint = req.headers[this.options.cookieKey] as string;
+      return {
+        fingerprint,
+        elementHash: { clientCookie: fingerprint },
+        headersHash: {}
+      };
+    }
+
+    if (req.headers[("x-" + this.options.cookieKey).toLowerCase()] != null) {
+      fingerprint = req.headers[
+        ("x-" + this.options.cookieKey).toLowerCase()
+      ] as string;
+      return {
+        fingerprint,
+        elementHash: { clientCookie: fingerprint },
+        headersHash: {}
+      };
+    }
+
+    let remoteAddress = req.headers["x-forwarded-for"];
+    if (!remoteAddress) {
+      remoteAddress = req.connection.remoteAddress;
+    }
+
+    let elementsHash = {
+      httpVersion: req.httpVersion,
+      remoteAddress: remoteAddress as string,
+      cookieKey: this.options.cookieKey,
+      hashedHostName: this.hashedHostName(),
+      remotePort: undefined,
+      rand: undefined,
+      time: undefined,
+      hashedPid: undefined
+    };
+
+    // these elements add greater entropy to the fingerprint, but aren't guaranteed to be the same upon each request
+    if (this.options.onlyStaticElements !== true) {
+      elementsHash.remotePort = req.connection.remotePort;
+      elementsHash.rand = Math.random();
+      elementsHash.time = new Date().getTime();
+      elementsHash.hashedPid = this.hashedPid();
+    }
+
+    for (const j in req.headers) {
+      if (
+        this.options.onlyStaticElements &&
+        (j === "cookie" || j === "referer")
+      ) {
+        continue;
+      }
+      key = "header_" + j;
+      elementsHash[key] = req.headers[j];
+    }
+
+    //@ts-ignore
+    elementsHash = this.sortAndStringObject(elementsHash);
+    fingerprint = this.calculateHashFromElements(elementsHash);
+
+    let headersHash = {};
+    if (this.options.toSetCookie === true) {
+      if (
+        this.options.settings !== undefined &&
+        this.options.settings !== null
+      ) {
+        let settingsParams = "";
+        /*
+        new version in object format e.g.
+        settings : {path : '/', expires : 86400000 }
+
+        old version till v0.0.4 e.g.
+        settings :'path=/;expires=' + new Date(new Date().getTime() + 86400000).toUTCString()+';'
+        */
+
+        /* Check to be compatible to both version */
+        if (typeof this.options.settings === "object") {
+          for (key in this.options.settings) {
+            if (
+              Object.prototype.hasOwnProperty.call(this.options.settings, key)
+            ) {
+              value = this.options.settings[key];
+              if (key === "expires") {
+                value = new Date(
+                  new Date().getTime() + this.options.settings[key]
+                ).toUTCString();
+              }
+              settingsParams =
+                settingsParams + key + (value ? "=" + value : "") + ";";
+            }
+          }
+        } else {
+          settingsParams = this.options.settings;
+        }
+        headersHash = {
+          "Set-Cookie":
+            this.options.cookieKey + "=" + fingerprint + ";" + settingsParams
+        };
+      } else {
+        headersHash = {
+          "Set-Cookie": this.options.cookieKey + "=" + fingerprint
+        };
+      }
+    }
+
+    return { fingerprint, elementsHash, headersHash };
+  }
+
+  hashedHostName() {
+    if (this.savedHashedHostName != null) {
+      return this.savedHashedHostName;
+    } else {
+      const md5sum = crypto.createHash("md5");
+      md5sum.update(os.hostname());
+      this.savedHashedHostName = md5sum.digest("hex");
+      return this.savedHashedHostName;
+    }
+  }
+
+  hashedPid() {
+    if (this.savedHashedPid != null) {
+      return this.savedHashedPid;
+    } else {
+      const md5sum = crypto.createHash("md5");
+      md5sum.update(String(process.pid));
+      this.savedHashedPid = md5sum.digest("hex");
+      return this.savedHashedPid;
+    }
+  }
+
+  parseCookies(req) {
+    const cookies = {};
+    if (req.headers.cookie != null) {
+      req.headers.cookie.split(";").forEach(cookie => {
+        const parts = cookie.split("=");
+        cookies[parts[0].trim()] = (parts[1] || "").trim();
+      });
+    }
+
+    return cookies;
+  }
+
+  calculateHashFromElements(elementsHash) {
+    const shaSum = crypto.createHash("sha1");
+    for (const i in elementsHash) {
+      shaSum.update(elementsHash[i]);
+    }
+
+    return shaSum.digest("hex");
+  }
+
+  sortAndStringObject(o: { [key: string]: any }) {
+    const sorted: { [key: string]: any } = {};
+    var key;
+    const a = [];
+
+    for (key in o) {
+      if (Object.prototype.hasOwnProperty.call(o, key)) {
+        a.push(key);
+      }
+    }
+
+    a.sort();
+
+    for (key = 0; key < a.length; key++) {
+      sorted[a[key]] = String(o[a[key]]);
+    }
+
+    return sorted;
+  }
+}
